1d450f21b249cc409f10e87d892a437e79557d7d
user <user@123.240.145.25>
2016-10-14 11:24:22 +0800

    不一樣就持續輸入版

--- src/fcu/iecs/opp/StringComparator.java
+++ src/fcu/iecs/opp/StringComparator.java
@@ -10,15 +10,11 @@ public class StringComparator {
 		str1 = sc.nextLine();
 		System.out.println("請輸入字串2");
 		str2 = sc.nextLine();
-		if (str1.equalsIgnoreCase(str2)) 
-		{
-			System.out.println("一樣");
-		} 
-		else 
-		{
-			System.out.println("不一樣");
+		while (!str1.equalsIgnoreCase(str2)) {
+			System.out.println("不一樣,請重新輸入字串2");
+			str2 = sc.nextLine();
 		}
-
+		System.out.println("一樣");
 		sc.close();
 
 	}
