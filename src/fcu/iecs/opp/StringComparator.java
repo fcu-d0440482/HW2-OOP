d17792617fd8ed52a77e176141dd53b32ec568ea
user <user@123.240.145.25>
2016-10-14 11:16:10 +0800

    HW2

--- src/fcu/iecs/opp/StringComparator.java
+++ src/fcu/iecs/opp/StringComparator.java
@@ -1,24 +1,21 @@
 
 import java.util.Scanner;
+
 public class StringComparator {
-	public static void main(String[] args)
-	{
-		String str1 , str2;
+	public static void main(String[] args) {
+		String str1, str2;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("請輸入字串1");
 		str1 = sc.nextLine();
 		System.out.println("請輸入字串2");
 		str2 = sc.nextLine();
-		while(!str1.equalsIgnoreCase(str2))
-		{
-			System.out.println("不一樣請重新輸入字串2");
-			str2 = sc.nextLine();
+		if (str1.equalsIgnoreCase(str2)) {
+			System.out.println("一樣");
+		} else {
+			System.out.println("不一樣");
 		}
-		System.out.println("一樣");
-		sc.close();	
-		
-		
+
+		sc.close();
 	}
-	
 
 }
